53fa7e8b14d8849aa26f430a93b7fdc5535241d5
aldajassofl <aldajassofl@ittepic.edu.mx>
2023-10-31 18:36:41 -0700

    Mostrar tareas, vista de agregar tareas

--- lib/BaseDatos.dart
+++ lib/BaseDatos.dart
@@ -1,6 +1,8 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
+import 'Tarea.dart';
+
 class DB{
   static Future<Database> _abrirBD() async{
     return openDatabase(
@@ -13,7 +15,29 @@ class DB{
               "Semestre text,"
               "Docente text,"
               ")"
+              "Create table Tarea("
+              "IDTarea text primary key,"
+              "IDMateria text,"
+              "F_entrega text,"
+              "Descripcion,"
+              "CONSTRAINT FK_TAREA_MATERIA FOREIGN KEY (IDMateria)"
+              ")"
         );
       },version: 1);
+  }
+  static Future<int> insertar(Tarea t) async{
+    Database db = await _abrirBD();
+    return db.insert("Tarea", t.toJSON(),conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+  static Future<List<Tarea>> mostrarTareas() async{
+    Database db = await _abrirBD();
+    List<Map<String,dynamic>> resultado = await db.query("Tarea");
+    return List.generate(resultado.length, (index){
+      return Tarea(
+          IDTarea: resultado[index]['IDTarea'],
+          IDMateria: resultado[index]['IDMateria'],
+          F_Entrega: resultado[index]['F_Entrega'],
+          Descripcion: resultado[index]['Descripcion']);
+    });
   }
 }
--- lib/aplicacion1.dart
+++ lib/aplicacion1.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'BaseDatos.dart';
+import 'Materia.dart';
+import 'Tarea.dart';
+
 class App01 extends StatefulWidget {
   const App01({super.key});
 
@@ -9,101 +13,252 @@ class App01 extends StatefulWidget {
 
 class _App01State extends State<App01> {
   int _index = 0;
+  final numTarea =  TextEditingController();
+  final descripcion = TextEditingController();
+  final materiaId = TextEditingController();
+  final fechaEntrega = TextEditingController();
+  String? materiaSeleccionada;
+  List<Tarea> Tareas = [
+    Tarea(
+        IDTarea: 1,
+        IDMateria: "1",
+        F_Entrega: "20/09/23",
+        Descripcion: "Tarea 1"),
+  ];
+
+  List<Materia> Materias = [
+    Materia(
+        IDMateria: "1", Nombre: "NoSQL", Semestre: "8", Docente: "Docente1"),
+    Materia(
+        IDMateria: "2", Nombre: "Moviles", Semestre: "9", Docente: "Docente2")
+  ];
+
+  Tarea TareaGlb =
+      Tarea(IDTarea: 0, IDMateria: "", F_Entrega: "", Descripcion: "");
+
+  DateTime f_entrega = DateTime.now();
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: f_entrega,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != f_entrega) {
+      setState(() {
+        f_entrega = picked;
+        fechaEntrega.text = "${f_entrega.toLocal()}".split(' ')[0];
+        print(fechaEntrega.text);
+      });
+    }
+  }
+
+  void actualizarListaTareas() async {
+    List<Tarea> temp = await DB.mostrarTareas();
+    setState(() {
+      Tareas = temp;
+    });
+  }
+
+  @override
+  void initState() {
+    actualizarListaTareas();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Practica 1"),),
+      appBar: AppBar(
+        title: Text("Practica 1"),
+      ),
       body: dinamico(),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.list),label: "Inicio"),
-        BottomNavigationBarItem(icon: Icon(Icons.add),label: "Materias"),
-        BottomNavigationBarItem(icon: Icon(Icons.add),label: "Tarea"),
-      ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: "Inicio"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Materias"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Tarea"),
+        ],
         currentIndex: _index,
-        onTap: (valor){
-        setState(() {
-          _index=valor;
-        });
+        onTap: (valor) {
+          setState(() {
+            _index = valor;
+          });
         },
       ),
     );
   }
 
-  Widget dinamico(){
-    switch(_index){
-      case 0:{
-        return Center(child: Text("Incio"),);
-      }
-      case 1:{
-        return DefaultTabController(length: 3,
-            child: Scaffold(
-              appBar: AppBar(
-                title: Text("Materias"),
-                bottom: TabBar(tabs: [
-                  Icon(Icons.list),
-                  Icon(Icons.edit),
-                  Icon(Icons.add)
+  Widget dinamico() {
+    switch (_index) {
+      case 0:
+        {
+          return Center(
+            child: Text("Incio"),
+          );
+        }
+      case 1:
+        {
+          return DefaultTabController(
+              length: 3,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text("Materias"),
+                  bottom: TabBar(tabs: [
+                    Icon(Icons.list),
+                    Icon(Icons.edit),
+                    Icon(Icons.add)
+                  ]),
+                ),
+                body: TabBarView(children: [
+                  //MOSTRAR TODOS
+                  ListView(
+                    children: [Text("Mostrar todos")],
+                  ),
+                  //EDITAR
+                  ListView(
+                    children: [Text("Editar")],
+                  ),
+                  //AGREGAR
+                  ListView(
+                    children: [Text("Agregar")],
+                  ),
                 ]),
-              ),
-              body: TabBarView(children: [
-                //MOSTRAR TODOS
-                ListView(
-                  children: [
-                    Text("Mostrar todos")
-                  ],
+              ));
+        }
+      case 2:
+        {
+          return DefaultTabController(
+              length: 3,
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text("Tareas"),
+                  bottom: TabBar(tabs: [
+                    Icon(Icons.list),
+                    Icon(Icons.edit),
+                    Icon(Icons.add)
+                  ]),
                 ),
-                //EDITAR
-                ListView(
-                  children: [
-                    Text("Editar")
-                  ],
-                ),
-                //AGREGAR
-                ListView(
-                  children: [
-                    Text("Agregar")
-                  ],
-                ),
-              ]),
-            ));
-      }
-      case 2:{
-        return DefaultTabController(length: 3,
-            child: Scaffold(
-              appBar: AppBar(
-                title: Text("Tareas"),
-                bottom: TabBar(tabs: [
-                  Icon(Icons.list),
-                  Icon(Icons.edit),
-                  Icon(Icons.add)
+                body: TabBarView(children: [
+                  //MOSTRAR TODOS
+                  mostrarTareas(),
+                  //EDITAR
+                  editarTareas(),
+                  //AGREGAR
+                  capturarTareas(),
                 ]),
-              ),
-              body: TabBarView(children: [
-                //MOSTRAR TODOS
-                ListView(
-                  children: [
-                    Text("Mostrar todos")
-                  ],
-                ),
-                //EDITAR
-                ListView(
-                  children: [
-                    Text("Editar")
-                  ],
-                ),
-                //AGREGAR
-                ListView(
-                  children: [
-                    Text("Agregar")
-                  ],
-                ),
-              ]),
-            ));
-      }
-      default:{
-        return Center();
-      }
+              ));
+        }
+      default:
+        {
+          return Center();
+        }
     }
   }
 
+  Widget mostrarTareas() {
+    return ListView.builder(
+        itemCount: Tareas.length,
+        itemBuilder: (context, indice) {
+          var materia = Materias.firstWhere(
+              (materia) => materia.IDMateria == Tareas[indice].IDMateria,
+              orElse: () => Materia(
+                  IDMateria: "",
+                  Nombre: "",
+                  Semestre: "",
+                  Docente: "")); //Buscar el nombre de la materia por IDMateria
+          return ListTile(
+            leading: CircleAvatar(
+              child: Text("${Tareas[indice].IDTarea}"),
+            ),
+            title: Text(
+                "${Tareas[indice].Descripcion} - Materia: ${materia != Materia(IDMateria: "", Nombre: "", Semestre: "", Docente: "") ? materia.Nombre : 'Materia Desconocida'}"),
+            subtitle: Text("${Tareas[indice].F_Entrega}"),
+            trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+            onTap: () {
+              setState(() {
+                TareaGlb = Tareas[indice];
+              });
+              descripcion.text = TareaGlb.Descripcion;
+              //f_entrega = TareaGlb.F_Entrega;
+              DefaultTabController.of(context).animateTo(1);
+            },
+          );
+        });
+  }
+
+  Widget capturarTareas() {
+    return ListView(
+      padding: EdgeInsets.all(40),
+      children: [
+        TextField(controller: numTarea,
+        decoration: InputDecoration(labelText: "Número de tarea"),keyboardType: TextInputType.number),
+        TextFormField(
+          readOnly: true,
+          controller: fechaEntrega,
+          onTap: () {
+            _selectDate(context);
+          },
+          decoration: InputDecoration(
+            labelText: 'Fecha',
+            suffixIcon: IconButton(
+              icon: Icon(Icons.calendar_today),
+              onPressed: () {
+                _selectDate(context);
+              },
+            ),
+          ),
+        ),
+        Text("${materiaId.text}"),
+        DropdownButton<String>(
+          hint: Text("Seleccionar Materia"),
+          value: materiaSeleccionada, // Valor actualmente seleccionado
+          onChanged: (String? value) {
+            setState(() {
+              materiaSeleccionada = value; // Actualiza la materia seleccionada
+            });
+          },
+          items: Materias.map((materia) {
+            return DropdownMenuItem<String>(
+              value:
+                  materia.Nombre, // Puedes usar el ID de la materia como valor
+              child: Text(materia.Nombre),
+              onTap: () {
+                setState(() {
+                  materiaId.text = materia.IDMateria;
+                });
+              },
+            );
+          }).toList(),
+        ),
+        TextField(
+          controller: descripcion,
+          decoration: InputDecoration(labelText: "Descripcion"),
+        ),
+        SizedBox(height: 30),
+        ElevatedButton(onPressed: (){
+          DB.insertar(
+            Tarea(
+                IDTarea: int.parse(numTarea.text),
+                IDMateria: materiaId.text,
+                F_Entrega: fechaEntrega.text,
+                Descripcion: descripcion.text)
+          );
+        }, child: Text("Agregar"))
+      ],
+    );
+  }
+
+  Widget editarTareas() {
+    return ListView(
+      padding: EdgeInsets.all(40),
+      children: [
+        Text("Editar Tarea ${TareaGlb.IDTarea}"),
+        TextField(
+          controller: descripcion,
+          decoration: InputDecoration(labelText: "Descripcion"),
+        ),
+      ],
+    );
+  }
 }
